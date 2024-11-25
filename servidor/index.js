6571a637809509fdeaa157298055fc11f179bab7
anajuaugusto <anajuliaaugusto068@gmail.com>
2024-11-25 09:42:30 -0300

    25/11

--- servidor/index.js
+++ servidor/index.js
@@ -16,8 +16,26 @@ const db = mysql.createConnetction({
 });
 
 //Conectando ao banco de dados
-db.connect((erro)) => {
-    if (erro){
-        console.error('Erro ao conectar ao Mysql:', erro)
+db.connect((erro) => {
+    if(erro) {
+        console.error('Erro ao conectar ao MySQL:',erro);
+    } else {
+        console.log('Conectando ao MySQl com sucesso');
+    
     }
-}
+});
+
+//Rota para cadastrar usuário
+app.post('/alunos', (req,res) => {
+    const {nome,cidade,estado} = req.body;
+
+    const sql = 'INSERT INTO alunos (nome,cidade,estado) VALUES (?,?,?)';
+
+    db.query(sql, [nome,cidade,estado],(err,result)) => {
+        if (err)
+        {
+            return res.status(500).json({ error: 'Erro ao cadastrar aluno !' });
+
+        }
+        res.status(201).json({ message: 'Aluno cadastro com sucesso!', id: result.insertId});
+    });
